08eb4bda2fd923ca5c16d7acb6be1791c89455b5
Todd Wolfson <todd@twolfson.com>
2017-11-30 16:14:57 -0800

    Dropped Safari and fetch check

--- src/utils/isIndexedDBValid.js
+++ src/utils/isIndexedDBValid.js
@@ -7,22 +7,8 @@ function isIndexedDBValid() {
         if (!idb) {
             return false;
         }
-        // We mimic PouchDB here;
-        //
-        // We test for openDatabase because IE Mobile identifies itself
-        // as Safari. Oh the lulz...
-        var isSafari = typeof openDatabase !== 'undefined' &&
-        /(Safari|iPhone|iPad|iPod)/.test(navigator.userAgent) &&
-        !/Chrome/.test(navigator.userAgent) &&
-        !/BlackBerry/.test(navigator.platform);
 
-        var hasFetch = typeof fetch === 'function' &&
-        fetch.toString().indexOf('[native code') !== -1;
-
-        // Safari <10.1 does not meet our requirements for IDB support (#5572)
-        // since Safari 10.1 shipped with fetch, we can use that to detect it
-        return (!isSafari || hasFetch) &&
-        typeof indexedDB !== 'undefined' &&
+        return typeof indexedDB !== 'undefined' &&
         // some outdated implementations of IDB that appear on Samsung
         // and HTC Android devices <4.4 are missing IDBKeyRange
         // See: https://github.com/mozilla/localForage/issues/128
